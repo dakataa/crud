c1d86ea554a1a1de58259569aae19475a6682402
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2023-12-19 10:22:41 +0200

    Default CRUD Layout & Navigation & Bundle Improvements

--- src/DakataaCrudBundle.php
+++ src/DakataaCrudBundle.php
@@ -2,14 +2,87 @@
 
 namespace Dakataa\Crud;
 
+use ReflectionClass;
+use Dakataa\Crud\Attribute\Navigation\NavigationGroup;
+use Dakataa\Crud\Attribute\Navigation\NavigationItem;
+use Dakataa\Crud\Service\Navigation;
+use Dakataa\Crud\Twig\Extension\CrudExtension;
+use Dakataa\Crud\Twig\Extension\NavigationExtension;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class DakataaCrudBundle extends Bundle
+class DakataaCrudBundle extends AbstractBundle
 {
 	const NAME = 'dakataa_crud';
-	
-	public function build(ContainerBuilder $container): void
+
+	public function configure(DefinitionConfigurator $definition): void
 	{
+		$definition
+			->rootNode()
+			->children()
+			->variableNode('layout')//->isRequired()
+			->end()
+			->end();
+	}
+
+	public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+	{
+		$container
+			->services()
+			->set(CrudExtension::class, CrudExtension::class)
+			->tag('twig.extension')
+			->autowire();
+
+		$container
+			->services()
+			->set(NavigationExtension::class, NavigationExtension::class)
+			->tag('twig.extension')
+			->autowire(true);
+
+		$container
+			->services()
+			->set(Navigation::class, Navigation::class)
+			->autowire(true)
+			->autoconfigure(true);
+
+		$container->parameters()->set(self::NAME, $config);
+
+		$builder->registerAttributeForAutoconfiguration(
+			NavigationItem::class,
+			static function (
+				ChildDefinition $definition,
+				NavigationItem $attribute,
+				ReflectionClass $reflector
+			): void {
+				$definition->addTag('dakataa.crud.navigation');
+			}
+		);
+
+		$builder->registerAttributeForAutoconfiguration(
+			NavigationGroup::class,
+			static function (
+				ChildDefinition $definition,
+				NavigationGroup $attribute,
+				ReflectionClass $reflector
+			): void {
+				$definition->addTag('dakataa.crud.navigation');
+			}
+		);
+	}
+
+	public function prependExtension(
+		ContainerConfigurator $containerConfigurator,
+		ContainerBuilder $containerBuilder
+	): void {
+		$containerBuilder->prependExtensionConfig('webpack_encore', [
+			'builds' => [
+				self::NAME => '%kernel.project_dir%/public/bundles/dakataacrud/assets',
+			],
+		]);
 	}
 }
